11d23654230d367a375903f86ce59f9491360837
Matt Myers <mrmyers1@fullsail.edu>
2019-07-17 15:57:23 -0400

    WebGL Serializing updated, Assets packed for streaming

--- Cubic/Assets/Scripts/Video_Player_Fix.cs
+++ Cubic/Assets/Scripts/Video_Player_Fix.cs
@@ -8,7 +8,7 @@ public class Video_Player_Fix : MonoBehaviour
     public VideoPlayer videoPlayer1, videoPlayer2, videoPlayer3, videoPlayer4, videoPlayer5, videoPlayer6;
     void Start()
     {
-        videoPlayer1.url = System.IO.Path.Combine(Application.streamingAssetsPath, "S1a");
+        videoPlayer1.url = System.IO.Path.Combine(Application.streamingAssetsPath, "S1a.mp4");
         /*videoPlayer2.url = System.IO.Path.Combine(Application.streamingAssetsPath, "02_10_Loop.mp4");
         videoPlayer3.url = System.IO.Path.Combine(Application.streamingAssetsPath, "03_45_Once.mp4");
         videoPlayer4.url = System.IO.Path.Combine(Application.streamingAssetsPath, "04_05_Loop.mp4");
