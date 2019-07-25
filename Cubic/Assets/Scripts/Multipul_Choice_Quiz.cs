4bd50d0d2fbd60ff48f470b3378ebe85c5dd5d41
Matt Myers <mrmyers1@fullsail.edu>
2019-07-25 02:55:27 -0400

    Quiz list filling out

--- Cubic/Assets/Scripts/Multipul_Choice_Quiz.cs
+++ Cubic/Assets/Scripts/Multipul_Choice_Quiz.cs
@@ -19,7 +19,7 @@ public class Multipul_Choice_Quiz : MonoBehaviour
         public bool correct;
     }
 
-    public List<Question> Question_List;
+    public List<Question> Question_List, Quiz_List;
     public GameObject Pause_Button, answer1_button, answer2_button, answer3_button;
     public TextMeshProUGUI Question_Text, answer1_text, answer2_text, answer3_text;
     bool triva_finished = false, triva_started = false;
@@ -31,8 +31,30 @@ public class Multipul_Choice_Quiz : MonoBehaviour
         Pause_Button.SetActive(triva_finished);
         if (triva_finished == false && triva_started == false)
         {
-
+            triva_started = true;
+            Triva_Go();
         }
+    }
+
+    private void Triva_Go()
+    {
+        int MaxNum = Question_List.Count;
+        for (int i = 0; i < 5; i++)
+        {
+            int tempnum = Random.Range(0, MaxNum);
+            Quiz_List.Add(Question_List[tempnum]);
+            Question_List.RemoveAt(tempnum);
+            MaxNum--;
+        }
+        next_question();
+    }
+
+    public void next_question()
+    {
+        Question_Text.SetText(Quiz_List[0].Question_text);
+        answer1_text.SetText(Quiz_List[0].Answers[0].Answer_text);
+        answer2_text.SetText(Quiz_List[0].Answers[1].Answer_text);
+        answer3_text.SetText(Quiz_List[0].Answers[2].Answer_text);
     }
 
     //EventSystem.current.currentSelectedGameObject.name
